32517a453599dd659006be4a1e7562bcafe34d49
raine <rainelmedina02@gmail.com>
2024-09-03 21:57:46 -0400

    Dise;o de la pantalla de usuarios

--- src/app/Mantenimiento/usuarios/usuarios.component.ts
+++ src/app/Mantenimiento/usuarios/usuarios.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.scss'
 })
